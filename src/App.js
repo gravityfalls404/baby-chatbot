5380ea7ee46bf6a1f93b9ce13bf771235e4d189d
Prashant <prashantg.co.17@nsit.net.in>
2020-09-29 20:39:26 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,125 @@
+import React, { Component } from 'react';
+import {Interactions} from 'aws-amplify';
+import {ChatFeed, Message} from 'react-chat-ui';
+
+
+
+
+// Imported default theme can be customized by overloading attributes
+const styles = {
+  bubbleStyles: {
+    text: {
+      fontSize: 16,
+    },
+    chatbubble: {
+      borderRadius: 30,
+      padding: 10
+    }
+  },
+  headerTitle: {
+    color: 'white',
+    fontSize: 22
+  },
+  header: {
+    backgroundColor: 'rgb(0, 132, 255)',
+    padding: 20,
+    borderTop: '12px solid rgb(204, 204, 204)'
+  },
+  messagesContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    padding: 10,
+    alignItems: 'center'
+  },
+  input: {
+    fontSize: 16,
+    padding: 10,
+    outline: 'none',
+    width: 350,
+    border: 'none',
+    borderBottom: '2px solid rgb(0, 132, 255)'
+  }
+}
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      input: '',
+        finalMessage: '',
+        messages: [
+          new Message({
+            id: 1,
+            message: "Hello, how can I help you today?",
+          })
+        ],
+    }
+  }
+  onChange(e){
+    const input = e.target.value
+    this.setState({
+      input
+    })
+  };
+  
+  _handleKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      this.submitMessage()
+    }
+  }
+  async submitMessage() {
+    const { input } = this.state
+    if (input === '') return
+    const message = new Message({
+      id: 0,
+      message: input,
+    })
+    let messages = [...this.state.messages, message]
+  
+    this.setState({
+      messages,
+      input: ''
+    })
+    const response = await Interactions.send("OrderFlowers_dev", input);
+    const responseMessage = new Message({
+      id: 1,
+      message: response.message,
+    })
+    messages  = [...this.state.messages, responseMessage]
+    this.setState({ messages })
+  
+    if (response.dialogState === 'Fulfilled') {
+      if (response.intentName === 'Greetings') {
+        const { slots: { Name, Age, House} } = response
+        const finalMessage = `It's Nice to meet you ${Name}, we can be good friends.`
+        this.setState({ finalMessage })
+      }
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+      <header style={styles.header}>
+        <p style={styles.headerTitle}>Welcome to my travel bot!</p>
+      </header>
+      <div style={styles.messagesContainer}>
+      <h2>{this.state.finalMessage}</h2>
+      <ChatFeed
+        messages={this.state.messages}
+        hasInputField={false}
+        bubbleStyles={styles.bubbleStyles}
+      />
+      <input
+        onKeyPress={this._handleKeyPress}
+        onChange={this.onChange.bind(this)}
+        style={styles.input}
+        value={this.state.input}
+      />
+      </div>
+    </div>
+    );
+  }
+}
+
+export default App;
